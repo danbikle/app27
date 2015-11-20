0c827ebb6c9e8234d14c9db5165bfb57579da503
Dan Bikle <dan.bikle@gmail.com>
2015-11-20 22:20:46 +0000

    more

---
+++ app27env.bash
@@ -0,0 +1,6 @@
+# I should dot this file instead of running it.
+# I use this file to setup my env-variables for ~/ann/app27
+export GEM_HOME=/home/ann/app27/gems
+export PATH=${PATH}:/home/ann/app22/node_modules/.bin
+export PATH=${GEM_HOME}/bin:${PATH}
+export PATH=${HOME}/heroku-client/bin:${PATH}
